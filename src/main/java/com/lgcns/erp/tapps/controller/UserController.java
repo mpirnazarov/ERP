aadb7f5a6f320f4483667cc42988ddbdca66fa9d
rafatdin <rafatdin@gmail.com>
2016-10-14 10:58:00 +0500

    registration - multi step

--- src/main/java/com/lgcns/erp/tapps/controller/UserController.java
+++ src/main/java/com/lgcns/erp/tapps/controller/UserController.java
@@ -2,6 +2,8 @@ package com.lgcns.erp.tapps.controller;
 
 
 import com.lgcns.erp.tapps.DbContext.UserService;
+import com.lgcns.erp.tapps.model.DbEntities.UserLocalizationsEntity;
+import com.lgcns.erp.tapps.model.DbEntities.UsersEntity;
 import com.lgcns.erp.tapps.model.UserInfo;
 import com.lgcns.erp.tapps.viewModel.LoginViewModel;
 import com.lgcns.erp.tapps.viewModel.RegistrationViewModel;
@@ -9,6 +11,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.json.simple.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
@@ -16,7 +19,7 @@ import org.springframework.web.servlet.View;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -55,12 +58,36 @@ public class UserController {
     }
 
     @RequestMapping (value = "/User/Register", method = RequestMethod.GET)
-    @ResponseBody public ModelAndView Register(){
+    @ResponseBody public ModelAndView Register(@ModelAttribute("registrationVM") RegistrationViewModel registrationViewModel ){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("user/register");
-        RegistrationViewModel registrationViewModel = new RegistrationViewModel();
         mav.addObject("registrationVM", registrationViewModel);
-        mav.addObject("heads", UserService.getDirectHeads());
+        Map<Integer, String> heads = new LinkedHashMap<Integer, String>();      //return data
+
+        Collection<UsersEntity> usersWithInfo = UserService.getDirectHeads();   //Getting list of users with localization info
+        Iterator itr = usersWithInfo.iterator();                                //info for iterator
+        UsersEntity userTemp;                                                   //info for iterator
+        UserLocalizationsEntity userLocTemp;                                    //info for iterator
+
+        while (itr.hasNext()) {
+            Object[] obj = (Object[]) itr.next();
+            userTemp =  (UsersEntity)obj[0];
+            userLocTemp =  (UserLocalizationsEntity)obj[1];
+            if(userLocTemp != null)
+                heads.put(userTemp.getId(),userLocTemp.getFirstName() + " " + userLocTemp.getFirstName());
+            else
+                heads.put(userTemp.getId(),userTemp.getUserName());
+        }
+        //mav.addObject("heads", UserService.getDirectHeads());
+        mav.addObject("heads", heads);
+        return mav;
+    }
+
+    @RequestMapping (value = "/User/Register", method = RequestMethod.POST)
+    @ResponseBody public ModelAndView RegisterPost(@ModelAttribute("registrationVM") RegistrationViewModel registrationViewModel ){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("user/register");
+
         return mav;
     }
 }
